4a55cf7c3019fbd473aa639f61732b6d35a8bc4a
Uday009 <asc11uday@gmail.com>
2024-12-10 13:35:34 +0530

    changed naming conventions

---
+++ LABS/React/reactapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App(){
+    return(
+        <div className='app-font'>
+            <h1>Welcome to react brooo!!!</h1>
+        </div>
+    )
+    
+}
+
+export default App;
